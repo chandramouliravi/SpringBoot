e588425daad5ae3b6f88fdec573bdfbb07c6c8f9
ravich <cravi@vertafore.com>
2022-12-18 20:29:17 +0530

    Initial Commit

---
+++ dynamoDbSpringBootDemo/src/main/java/com/ravich/controller/employeeController.java
@@ -0,0 +1,35 @@
+package com.ravich.controller;
+
+import com.ravich.entity.Employee;
+import com.ravich.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class employeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @PostMapping("/employee")
+    public Employee saveEmployee(@RequestBody Employee employee){
+        return  employeeRepository.save(employee);
+    }
+
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") String employeeId){
+        return  employeeRepository.getEmployeeById(employeeId);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public String deleteEmployee(@PathVariable("id") String employeeId){
+        return  employeeRepository.delete(employeeId);
+    }
+
+    @PutMapping("/employee/{id}")
+    public String updateEmployee(@PathVariable("id") String employeeId, @RequestBody Employee employee){
+        return  employeeRepository.update(employeeId, employee);
+    }
+
+}
